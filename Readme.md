e0792282e26a6eede1c0a4859dab11b534703e3e
Vijay-Hozo <vijay2304a@gmail.com>
2024-09-11 22:02:32 +0530

    Success

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# 👋 Introducing Myself in Style: Terminal Edition! 🚀
+
+Hello Tech community! I'm excited to share a fun and creative project I've been working on – an introduction of myself right in your terminal. 😄👨‍💻
+
+
+### **What is it?**
+
+I've developed this project using JavaScript, with a little help from the fantastic Chalk.js and Boxen libraries. It's a unique way to give your terminal a personal touch and make your introduction stand out.
+
+### **Why Terminal?**
+
+We often introduce ourselves through social media profiles, but why not do it in style, right from your command line? It's a fun way to add a personal touch to your developer identity.
+
+### **How to Try It?**
+
+To give it a whirl, follow these simple steps:
+
+1. Make sure you have Node.js and npm installed on your laptop or computer. If you don't have npm yet, you can get it here: [https://www.npmjs.com/get-npm](https://www.npmjs.com/get-npm).
+
+2. Open your terminal and simply enter the following command: ```npx vijay```
+
+Your introduction will appear in all its terminal glory. 🌟
+
+Thank you for trying it out, and happy coding! 💻😊
---
+++ bin/index.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import boxen from "boxen";
+
+const options = {
+  title: " Hello there ! ",
+  titleAlignment: "center",
+  borderStyle: "round",
+  width: 75,
+  padding: 1,
+  borderColor: "#A7FF83",
+};
+
+const data = {
+  intro: chalk
+    .hex("#FFFADD")
+    .bold("I'm Vijay, your friendly neighbourhood developer."),
+  roles: chalk.hex("#97FFF4")(
+    "FullStack Developer | UI/UX Designer | Pursuing BTech IT | Open Source Enthusiast | Pre-final year student | Tech Enthusiast | Freelancer"
+  ),
+  twitter:
+    chalk.white.bold("https://twitter.com/") + chalk.cyan("mr_vijay_a_"),
+  github: chalk.white.bold("https://github.com/") + chalk.green("Vijay-Hozo"),
+  linkedin:
+    chalk.white.bold("https://linkedin.com/in/") + chalk.blue("mr-vijay"),
+  Socials: chalk.white.bold("https://shor.by/") + chalk.cyan("vijay-a"),
+  Portfolio:
+    chalk.white.bold("https://") + chalk.hex("#97FFF4")("avijay.tech"),
+  labelTwitter: chalk.bgHex("#FFF").hex("#00acee").bold("    Twitter"),
+  labelGitHub: chalk.bgBlack.white.bold("     GitHub"),
+  labelLinkedIn: chalk.bgHex("#0072b1").hex("#fef9f3").bold("   LinkedIn"),
+  labelWeb: chalk.bgHex("#FFE7CE").hex("#071A52").bold("    Socials"),
+  labelPortfolio: chalk.bgHex("#9400FF").hex("#fef9f3").bold("  Portfolio"),
+};
+
+const newline = "\n";
+const introduction = `${data.intro}`;
+const roles = `${data.roles}`;
+const twittering = `${data.labelTwitter}  ${data.twitter}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+const webing = `${data.labelWeb}  ${data.Socials}`;
+const portfolio = `${data.labelPortfolio}  ${data.Portfolio}`;
+
+const output =
+  newline +
+  newline +
+  newline +
+  introduction +
+  newline +
+  roles +
+  newline +
+  newline +
+  newline +
+  twittering +
+  newline +
+  githubing +
+  newline +
+  linkedining +
+  newline +
+  webing +
+  newline +
+  portfolio +
+  newline +
+  newline +
+  newline;
+
+console.log(boxen(chalk.white(output), options));
