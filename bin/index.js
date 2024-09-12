b2a50072ad84e5ba11656a6956973364a97a8f56
Vijay-Hozo <vijay2304a@gmail.com>
2024-09-12 09:50:04 +0530

    success

--- bin/index.js
+++ bin/index.js
@@ -25,12 +25,12 @@ const data = {
   Socials: chalk.white.bold("https://shor.by/") + chalk.cyan("vijay-a"),
   Portfolio: chalk.white.bold("https://") + chalk.hex("#97FFF4")("avijay.tech"),
   gmail: chalk.red("avijay.personal@gmail.com"),
-  labelTwitter: chalk.bgHex("#FFF").hex("#00acee").bold("    Twitter"),
+  labelTwitter: chalk.bgHex("#FFF").hex("#00acee").bold("           Twitter"),
   labelGitHub: chalk.bgBlack.white.bold("     GitHub"),
-  labelLinkedIn: chalk.bgHex("#0072b1").hex("#fef9f3").bold("   LinkedIn"),
-  labelWeb: chalk.bgHex("#FFE7CE").hex("#071A52").bold("    Socials"),
-  labelPortfolio: chalk.bgHex("#9400FF").hex("#fef9f3").bold("  Portfolio"),
-  labelGmail: chalk.bgRed.white.bold(" Gmail "),
+  labelLinkedIn: chalk.bgHex("#0072b1").hex("#fef9f3").bold("         LinkedIn"),
+  labelWeb: chalk.bgHex("#FFE7CE").hex("#071A52").bold("            Socials"),
+  labelPortfolio: chalk.bgHex("#9400FF").hex("#fef9f3").bold("      Portfolio"),
+  labelGmail: chalk.bgRed.white.bold("      Gmail "),
 };
 
 const newline = "\n";
